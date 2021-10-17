c6a290666175af33a292686b92c68368b966fb84
Zeki Zheng <798012131@qq.com>
2021-10-18 00:45:16 +0800

    Init Commit

---
+++ src/main/java/com/zekizheng/trading/UniversityTradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zekizheng.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityTradingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityTradingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zekizheng/trading/UniversityTradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zekizheng.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityTradingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
